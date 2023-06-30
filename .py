a3dec310f907360442bf716b561751eaff20bacc
VaKo2179 <115894285+VaKo2179@users.noreply.github.com>
2023-06-30 11:13:38 +0200

    Update .py

--- .py
+++ .py
@@ -1,19 +1,1 @@
-dict1 = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
 
-dict2 = {
-    "water": 100,
-    "milk": 50,
-    "coffee": 75,
-}
-
-result = {}
-
-for key in dict1:
-    if key in dict2:
-        result[key] = dict1[key] - dict2[key]
-
-print(result)
